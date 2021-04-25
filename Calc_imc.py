490d0f4507d1be07c22b9b822f9d1a4b674949ca
gabrielvpc <53542803+gabrielvpc@users.noreply.github.com>
2021-04-25 15:37:33 -0300

    Update Calc_imc.py

--- Calc_imc.py
+++ Calc_imc.py
@@ -1,22 +1,16 @@
-#Conexão com o banco mysql
 import mysql.connector
 from os import system
 conn = mysql.connector.connect(user='root', password='root', host='127.0.0.1', database='calc_imc')
 cursor = conn.cursor()
-#Conexão com o banco mysql
-cursor.execute("USE calc_imc;")
-
-#i = 1
-#while i == 1:
-
+system("clear")
 print("\nSelecione uma das opções:\n\n1 = Listar usuarios \n2 = Adicionar usuarios \n3 = Calculadora de IMC\n")
 opcao = int(input("> "))
 if opcao == 1:
 	cursor.execute("SELECT * FROM pessoas;")
 	printar = cursor.fetchall()
-	print("\nNOME - PESO - ALTURA")
+	print("\nID - NOME - PESO - ALTURA")
 	for comando in printar:
-		print(comando[1],comando[2],comando[3])
+		print(comando[0],comando[1],comando[2],comando[3])
 elif opcao == 2:
 	print("\nInsira seu nome")
 	nome = str(input("> "))
